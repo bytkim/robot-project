caf664d155a33ab8de41e620964ac96c7262ac8c
AnreThunderson <47703955+AnreThunderson@users.noreply.github.com>
2026-04-09 18:25:45 -0700

    Update gotobeacon.c

--- src/gotobeacon.c
+++ src/gotobeacon.c
@@ -2,7 +2,7 @@
 #pragma config(Sensor, dgtl10, digital10, sensorDigitalOut)
 #pragma config(Sensor, dgtl11, digital11, sensorDigitalOut)
 #pragma config(Sensor, dgtl12, digital12, sensorDigitalOut)
-#pragma config(Sesnor, dgtll8, sensorPort, sensorPort)
+#pragma config(Sensor, dgtl8, digital8, sensorPort)
 #pragma config(Motor, port1, rightMotor, tmotorVex393_HBridge, openLoop, reversed)
 #pragma config(Motor, port10, leftMotor, tmotorVex393_HBridge, openLoop, reversed)
 #pragma config(Motor, port9, clawMotor, tmotorVex393_HBridge, openLoop, reversed) // NOTE: ARM CONFIG (UNVERIFIED)
